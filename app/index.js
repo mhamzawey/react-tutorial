78f38bd50589e4fb9a1f89fa8e881077afec3532
mhamzawey <mac@Macs-Mac-mini.local>
2018-10-14 01:43:15 +0200

    stack initialization

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { SignedOut,SignedIn } from "./router";
+
+export default class App extends React.Component {
+  render() {
+    return <SignedOut />;
+  }
+}
---
+++ app/router.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { StackNavigator,TabNavigator } from "react-navigation";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import FontAwesome from "react-native-fontawesome";
+
+export const SignedOut = StackNavigator({
+  SignUp: {
+    screen: SignUp,
+    navigationOptions: {
+      title: "Sign Up"
+    }
+  },
+  SignIn: {
+    screen: SignIn,
+    navigationOptions: {
+      title: "Sign In"
+    }
+  }
+});
+
+
+export const SignedIn = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="home" size={30} color={tintColor} />
+      )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="user" size={30} color={tintColor} />
+      )
+    }
+  }
+});
